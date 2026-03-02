0af0ebb1e1e394ee1e9242a47b886382252c1722
IsRylo <91057829+IsRylo@users.noreply.github.com>
2026-03-02 11:51:06 +0800

    Code cleanup

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Function called whenever the URL has changes
 function router() {
+    console.log("router called");
     // Initialize the hash, if null then #home
     const hash = window.location.hash || '#home';
     switch (hash) {
@@ -19,5 +20,22 @@ window.addEventListener('load', router);
 
 
 function renderHome() {
+    document.getElementById('app').innerHTML = `
+        <section class="hero">
+            <div class="hero-photo">
+                <img src="images/photo.jpg" alt="Your Name" />
+            </div>
+            <div class="hero-text">
+                <h1>Hello</h1>
+                <h2>A Bit About Me</h2>
+                <p>Your description here.</p>
+                <div class="hero-buttons">
+                <a href="#resume"   class="btn btn-yellow">Resume</a>
+                <a href="#projects" class="btn btn-red">Projects</a>
+                <a href="#contact"  class="btn btn-teal">Contact</a>
+                </div>
+            </div>
+        </section>
+    `;
 
 }
